50c5308a19114ac275249794f1b9f09723609aeb
Frédéric Darré <17145256+fdarre@users.noreply.github.com>
2023-08-22 22:06:05 +0200

    Handle basic unit movement

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,18 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Serialized Fields
+
+    [SerializeField] private float moveSpeed = 4f;
+
+    #endregion
+
+    #region Update
+
+    private void Update()
     {
-        
+        float stoppingDistance = 0.1f;
+
+        if (Vector3.Distance(transform.position, _targetPosition) > stoppingDistance)
+        {
+            Vector3 moveDirection = _targetPosition - transform.position.normalized;
+            transform.position += moveDirection * (moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Move(new Vector3(4, 0, 4));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    #endregion
+
+    #region Private Methods
+
+    private void Move(Vector3 targetPosition)
     {
-        
+        _targetPosition = targetPosition;
     }
+
+    #endregion
+
+    #region Private Fields
+
+    private Vector3 _targetPosition;
+
+    #endregion
 }
