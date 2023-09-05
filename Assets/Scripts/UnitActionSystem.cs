4e70fac7a9eb5d9332713d6ee84be6c4d655899b
Frédéric Darré <17145256+fdarre@users.noreply.github.com>
2023-09-05 11:52:56 +0200

    Handle unit selection visual

--- Assets/Scripts/UnitActionSystem.cs
+++ Assets/Scripts/UnitActionSystem.cs
@@ -1,12 +1,31 @@
+using System;
 using UnityEngine;
 
 public class UnitActionSystem : MonoBehaviour
 {
-    [SerializeField] private Unit selectedUnit;
+
+    public static UnitActionSystem Instance { get; private set; }
+
+    public Unit selectedUnit { get; private set; }
+
+    public event EventHandler OnSelectedUnitChanged;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("There is more than one unit action system.");
+            Destroy(this.gameObject);
+            return;
+        }
+        Instance = this;
+
+        selectedUnit = FindAnyObjectByType<Unit>();
+    }
+
 
     private void Update()
     {
-
         if (Input.GetMouseButtonDown(0))
         {
             if(TryHandleUnitSelection()) return;
@@ -22,10 +41,22 @@ public class UnitActionSystem : MonoBehaviour
         {
             if(hit.collider.TryGetComponent(out Unit clickedUnit))
             {
-                selectedUnit = clickedUnit;
+                SetSelectedUnit(clickedUnit);
                 return true;
             }
         }
         return false;
     }
+
+    private void SetSelectedUnit(Unit unit)
+    {
+        selectedUnit = unit;
+
+        //Null check to avoid null reference exception if there is no event subscribers
+        if (OnSelectedUnitChanged != null)
+        {
+            OnSelectedUnitChanged(this, EventArgs.Empty);
+        }
+    }
+
 }
